ac137f63c0094269f9684ccfbc6a4a668fde208b
kaliIinux <>
2023-07-31 11:23:18 -0300

    att

--- next-react/project-1/src/App.jsx
+++ next-react/project-1/src/App.jsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Component } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+class App extends Component {
+
+  state = {
+    count: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'O título 1',
+        body: 'O cortpo 1'
+      },
+      {
+        id: 2,
+        title: 'O título 2',
+        body: 'O cortpo 2'
+      },
+      {
+        id: 3,
+        title: 'O título 3',
+        body: 'O cortpo 3'
+      },
+    ]
+  }
+
+  componentDidMount() {
+    this.handleTimeOut()
+  }
+
+  componentDidUpdate(){
+    this.handleTimeOut()
+  }
+
+  handleTimeOut = () => {
+    const { posts, count } = this.state
+    posts[0].title = 'O título mudou'
+    
+
+    setTimeout(() => {
+      this.setState({posts, count: count + 1})
+    }, 0.00001)
+  }
+
+  render() {
+
+    const { posts, count } = this.state
+
+    return (
+      <div className='App'>
+        <h1>{count}</h1>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
+  }
 }
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
 
 export default App
